8065a278cc4851733400f54d80d8144b4218c62d
letha <no email>
2020-06-11 11:40:22 +0000

    Add migrations.

---
+++ src/Migrations/Version20200611063649.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200611063649 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_64C19C1B4BCC40F ON category (e_id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_D34A04ADB4BCC40F ON product (e_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX UNIQ_64C19C1B4BCC40F ON category');
+        $this->addSql('DROP INDEX UNIQ_D34A04ADB4BCC40F ON product');
+    }
+}
